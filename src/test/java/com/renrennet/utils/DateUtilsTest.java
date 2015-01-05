3aad3c841f29fcccf22658be1d7a4f6aed7d67d0
leiguorui <degreelei@gmail.com>
2015-01-05 12:12:27 +0800

    file download from url

--- src/test/java/com/renrennet/utils/DateUtilsTest.java
+++ src/test/java/com/renrennet/utils/DateUtilsTest.java
@@ -1,5 +1,6 @@
 package com.renrennet.utils;
 
+import com.renrennet.utils.datetime.DataConvert;
 import com.renrennet.utils.datetime.DateUtils;
 import org.junit.Test;
 
@@ -9,7 +10,7 @@ import org.junit.Test;
 public class DateUtilsTest {
     @Test
     public void test(){
-        System.out.println(DateUtils.getLastTime(18720000));
-        System.out.println(DateUtils.addDaysToCurrent(10));
+        System.out.println(DataConvert.getLastTime(18720000));
+        System.out.println(DataConvert.addDaysToCurrent(10));
     }
 }
