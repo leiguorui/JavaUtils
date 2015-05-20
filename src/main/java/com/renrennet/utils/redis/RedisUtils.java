ce33c468801a77e460d930347c200157c1e4233c
leiguorui <degreelei@gmail.com>
2015-05-20 14:54:48 +0800

    redis

--- src/main/java/com/renrennet/utils/redis/RedisUtils.java
+++ src/main/java/com/renrennet/utils/redis/RedisUtils.java
@@ -1,5 +1,6 @@
 package com.renrennet.utils.redis;
 
+import com.renrennet.utils.io.SerializeUtil;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
@@ -9,6 +10,9 @@ import java.util.Set;
 
 /**
  * redis 的基本操作
+ *
+ * redis的读写操作，使用单例合适吗？
+ *
  * User: Administrator
  * Date: 15-3-16
  * Time: 下午5:55
@@ -35,6 +39,9 @@ public class RedisUtils {
         return jedis;
     }
 
+    /**
+     * 查询所有的key
+     */
     public void getKeys(){
         System.out.println("Connection to server sucessfully");
         //store data in redis list
@@ -46,13 +53,48 @@ public class RedisUtils {
         }
     }
 
-    public static void main(String[] args){
-//        Jedis jedis = RedisUtils.getInstance().getJedis();
-//
-////        jedis.set("car_match_hello", "bar");
-//        String value = jedis.get("car_match_hello");
-//        System.out.println(value);
+    /**
+     * 根据Key获取值
+     * @param key
+     * @return
+     */
+    public Object getValue(String key){
+        byte[] getResult = null;
 
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            getResult = jedis.get(key.getBytes());
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+
+        Object result =  SerializeUtil.unserialize(getResult);
+
+        return result;
+    }
+
+    /**
+     * 设置值
+     * @param key
+     * @param value
+     */
+    public void setValue(String key, Object value){
+        System.out.println("-----添加一条记录------"+key.toString()+"------------");
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            jedis.set(key.getBytes(), SerializeUtil.serialize(value));
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public static void main(String[] args){
         RedisUtils.getInstance().getKeys();
     }
 }
