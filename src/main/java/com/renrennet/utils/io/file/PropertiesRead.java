7daeb7cf181687f6ccf736d2bf9370933ddca6d2
leiguorui <degreelei@gmail.com>
2015-05-20 14:27:11 +0800

    改为单例

--- src/main/java/com/renrennet/utils/io/file/PropertiesRead.java
+++ src/main/java/com/renrennet/utils/io/file/PropertiesRead.java
@@ -6,26 +6,21 @@ import java.io.InputStream;
 import java.util.Properties;
 
 /**
+ * 为了properties文件只被读取一次，所有使用了单例，在创建实例时读取properties
  * User: Green lei
  * Date: 15-4-22
  * Time: 上午11:54
  */
 public class PropertiesRead {
-    public static String getValue(String key){
-        Properties prop = new Properties();
-        InputStream input = null;
-        String value = null;
+    private static PropertiesRead instance = null;
+    private static Properties prop = new Properties();
+    InputStream input = null;
 
+    protected PropertiesRead() {
         try {
-
             input = PropertiesRead.class.getResourceAsStream("/paramter.properties");
-
             // load a properties file
             prop.load(input);
-
-            // get the property value and print it out
-            value = prop.getProperty(key);
-
         } catch (IOException ex) {
             ex.printStackTrace();
         } finally {
@@ -37,6 +32,22 @@ public class PropertiesRead {
                 }
             }
         }
+        // Exists only to defeat instantiation.
+    }
+
+    public static PropertiesRead getInstance() {
+        if(instance == null) {
+            instance = new PropertiesRead();
+        }
+        return instance;
+    }
+
+    public String getValue(String key){
+
+        String value = null;
+
+        // get the property value and print it out
+        value = prop.getProperty(key);
 
         return value;
     }
